6aa893a33db2781befc58a9992106619053b99c7
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2017-04-15 23:19:00 -0400

    fix translate

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-var app = angular.module('app', ['ui.bootstrap']);
-
+var app = angular.module('app', ['ui.bootstrap','pascalprecht.translate']);
+app.config(['$translateProvider', function($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+      files: [{
+          prefix: '/i18n/category/locale-',
+          suffix: '.json'
+      }, {
+          prefix: '/i18n/product/locale-',
+          suffix: '.json'
+      }]
+  });
+  $translateProvider.preferredLanguage('en');  
+  $translateProvider.useSanitizeValueStrategy('escape');
+}]);
