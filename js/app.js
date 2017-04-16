99e2dcda463d175a7ed0e58dc96668ae6f35beba
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2017-04-15 23:34:55 -0400

    fix all

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-var app = angular.module('app', ['ui.bootstrap','pascalprecht.translate']);
-app.config(['$translateProvider', function($translateProvider) {
-  $translateProvider.useStaticFilesLoader({
-      files: [{
-          prefix: '/i18n/category/locale-',
-          suffix: '.json'
-      }, {
-          prefix: '/i18n/product/locale-',
-          suffix: '.json'
-      }]
-  });
-  $translateProvider.preferredLanguage('en');  
-  $translateProvider.useSanitizeValueStrategy('escape');
+var app = angular.module('app', ['ui.bootstrap','pascalprecht.translate','ngRoute']);
+app.config(['$translateProvider','$routeProvider', function($translateProvider,$routeProvider) {
+    $translateProvider.useStaticFilesLoader({
+        files: [{
+            prefix: '/i18n/category/locale-',
+            suffix: '.json'
+        }, {
+            prefix: '/i18n/product/locale-',
+            suffix: '.json'
+        }]
+    });
+    $translateProvider.preferredLanguage('en');  
+    $translateProvider.useSanitizeValueStrategy('escape');
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.html"
+    })
+    .when("/women", {
+        templateUrl : "women.html"
+    })
+    .when("/children", {
+        templateUrl : "children.html"
+    })
+    .when("/men", {
+        templateUrl : "men.html"
+    })
+    .when("/groupon", {
+        templateUrl : "groupon.html"
+    })    
+    ;  
 }]);
