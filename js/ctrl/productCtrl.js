8b740aa490c1f7635db92159fa8acc2f959a9796
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2017-04-28 23:25:51 -0400

    fix all

--- js/ctrl/productCtrl.js
+++ js/ctrl/productCtrl.js
@@ -1,6 +1,5 @@
 app.controller('productCtrl', function ($scope,DataManager, $routeParams) {
 
-
   DataManager.loadAll().then(function(data) {
   	  id = $routeParams.id;
   	  console.log("id="+id);
@@ -8,6 +7,17 @@ app.controller('productCtrl', function ($scope,DataManager, $routeParams) {
       console.log(products);
       $scope.product = products[id];
       console.log($scope.product);
+      side_path = $scope.product.side_path;
+      side_path[0].active="active";
   });
+
+$scope.active1 = "active";
+$scope.setActive = function(item) {
+	side_path = $scope.product.side_path;
+	for (i = 0; i < side_path.length; i++) { 
+	    side_path[i].active="";
+	}	
+    item.active="active";
+}
  
 });
