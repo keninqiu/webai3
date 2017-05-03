c664af66fc6b44d00f70e3482b0e2eeacd21d321
Kening Qiu <keningqiu@Kenings-MacBook-Pro.local>
2017-05-02 23:18:10 -0400

    fix

--- admin/config/db.php
+++ admin/config/db.php
@@ -1,9 +1,10 @@
 <?php
-
+require_once __DIR__ . '/../../class/Config.class.php';
+use Webai\Api\Config;
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=webai',
-    'username' => 'root',
-    'password' => 'mysql',
+    'dsn' => 'mysql:host=localhost;dbname='.Config::DB_HOST,
+    'username' => Config::DB_USER,
+    'password' => Config::DB_PASS,
     'charset' => 'utf8',
 ];
